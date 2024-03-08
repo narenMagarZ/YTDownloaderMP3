cf8b4deb6f3415c7af521899e28a5da18b78446a
narenMagarZ <narenmagarz98@gmail.com>
2024-03-08 20:45:08 +0545

    Implement video-card and attach the check-box clicked event

--- src/widgets/list.py
+++ src/widgets/list.py
@@ -5,77 +5,90 @@ from .card import *
 from PyQt5.QtGui import QPixmap
 
 
-
-def handleResponse(response:QNetworkReply,thumbnail:QLabel):
-    if response.error() == QNetworkReply.NoError:
-        data = response.readAll()
-        pixmap = QPixmap()
-        pixmap.loadFromData(data)
-        scaledPixmap = pixmap.scaled(QSize(100,100),Qt.KeepAspectRatio)
-        thumbnail.setPixmap(scaledPixmap)
-    else:
-        # handle network errors
-        print(f"Error fetching image:",response.errorString())
-
 class CustomList(QWidget):
-    def __init__(self,items):
+    def __init__(self,filter,items,videoCards):
         super().__init__()
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
-        self.networkManager = QNetworkAccessManager()
-        for imageUrl, title in items:
-            card = QHBoxLayout()
-            card.setAlignment(Qt.AlignLeft)
-            card.setContentsMargins(0,5,0,0)
-            videoTitle = QLabel(title)
-            checkbox = QCheckBox()
-            checkbox.setChecked(True)
-            thumbnail = QLabel()
-            request = QNetworkRequest(QUrl(imageUrl))
-            response = self.networkManager.get(request)
-            response.finished.connect(lambda:handleResponse(response,thumbnail))
-            card.addWidget(checkbox)
-            card.addWidget(thumbnail)
-            card.addWidget(videoTitle)
-            self.layout.addLayout(card)
+        self.cardCheckState = []
+        self.cards = []
+        def handleClick(state):
+            for card in videoCards:
+                checkbox = card["checkbox"]
+                if checkbox:
+                    checkbox.setChecked(state)
+        selectAllBox = filter.widgets[0]
+        # attach the click event to the selectAll checkbox
+        if selectAllBox and isinstance(selectAllBox,(QCheckBox)):
+            selectAllBox.clicked.connect(handleClick)
+
+        # create the cards for each video item
+        for i,(imageUrl, title) in enumerate(items):
+            self.cardCheckState.append(True) # by default the state is true for each card
+            card = VideoInfoCard(imageUrl,title,filter,self,i,videoCards)
+            # self.cards.append(card)
+            self.layout.addWidget(card)
 
 
 
-def handleReply(reply:QNetworkReply):
-    try:
+def handleReply(reply:QNetworkReply,videoThumbnail):
+    if reply.error() == QNetworkReply.NoError:
         data = reply.readAll()
         pixmap = QPixmap()
         pixmap.loadFromData(data)
-        scaledPixmap = pixmap.scaled(QSize(100,100,Qt.keepAspectRatio))
-        
-        return data
-    except Exception as e:
-        print(f"Error fetching image:",e)
+        scaledPixmap = pixmap.scaled(QSize(100,100))
+        videoThumbnail.setPixmap(scaledPixmap)
+    else:
+        print(f"Error fetching image:",reply.errorString())
 
-def fetchImage(imageUrl,networkManager):
+def fetchImage(imageUrl,networkManager,videoThumbnail):
     request = QNetworkRequest(QUrl(imageUrl))
     reply = networkManager.get(request)
-    reply.finished.connect(lambda:handleResponse(reply))
+    if reply:
+        reply.finished.connect(lambda:handleReply(reply,videoThumbnail))
 
-class VideoInfoCard(QVBoxLayout):
-    def __init__(self,imageUrl,title):
+class VideoInfoCard(QWidget):
+    def __init__(self,imageUrl,title,filter,cList,index,videoCards):
+        super().__init__()
+        self.index = index
+        self.filter = filter
+        self.cList = cList
         self.networkManager = QNetworkAccessManager()
         card = QVBoxLayout()
+        self.setLayout(card)
         card.setAlignment(Qt.AlignLeft)
         card.setContentsMargins(0,5,0,0)
-
+        self.videoCards = videoCards
         div1 = QHBoxLayout()
         checkbox = QCheckBox()
+        checkbox.setObjectName(f"ch_check{index}")
         checkbox.setChecked(True)
-        title = QLabel(title)
-        thumbnail = QLabel()
-        fetchImage(imageUrl,self.networkManager)
+        checkbox.clicked.connect(self.__handleCheckBoxClick)
+        # id,yt keys are already set
+        videoCards[index]["checkState"] = True
+        videoCards[index]["checkbox"]=checkbox
+        videoTitle = QLabel(title)
+        videoThumbnail = QLabel()
+        fetchImage(imageUrl,self.networkManager,videoThumbnail)
 
+        div1.addWidget(checkbox)
+        div1.addWidget(videoThumbnail)
+        div1.addWidget(videoTitle)
         div2 = QHBoxLayout()
         
-
-
         card.addLayout(div1)
         card.addLayout(div2)
-        pass
+    
+    def __isAllBoxChecked(self,box):
+        prev = True
+        for i in box:
+            prev = prev & i
+        return prev
+    def __handleCheckBoxClick(self,state):
+        self.videoCards[self.index]["checkState"]=state
+        checkState = [item["checkState"] for item in self.videoCards]
+        isAllBoxChecked = self.__isAllBoxChecked(checkState)
+        self.filter.selectAll = isAllBoxChecked
+        selectAllBox = self.filter.widgets[0]
+        selectAllBox.setChecked(isAllBoxChecked)
 
