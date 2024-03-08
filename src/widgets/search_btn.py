a2f7cd76add251ba39129537e760280723eac72f
narenMagarZ <narenmagarz98@gmail.com>
2024-03-08 20:46:36 +0545

    Add videocards obj to store the reference to each video card from the search and implement download function

--- src/widgets/search_btn.py
+++ src/widgets/search_btn.py
@@ -1,55 +1,67 @@
 from PyQt5.QtWidgets import QPushButton, QVBoxLayout
 from pytube import YouTube
 from .list import *
-
-
-def downloadProgress(stream,chunk,bytes):
-    pass
+from .helper.generate_folderpath import generateFolderPath
+import threading
 
 
 class SearchBtn():
     inputBox = None
     mainLayout = None
     filter = None
-    youtube = []
+    yts = []
+    videoCards = []
     def __init__(self,inputbox,mainLayout:QVBoxLayout):
         self.inputBox = inputbox
         self.mainLayout = mainLayout
+    def __downloadProgress(self,stream,chunk,bytesRemaining):
+        totalSize = stream.filesize
+        bytesDownloaded = totalSize - bytesRemaining
+        percentageCompleted = (bytesDownloaded / totalSize ) * 100
+        print(f"download progress: {round(percentageCompleted,2)}%")
     def getVideoInfo(self,url):
         try:
             yt = YouTube(url)
-            self.youtube.append(yt)
             title = yt.title
             thumbnail = yt.thumbnail_url
-            print(title,thumbnail)
             return(thumbnail,title)
         except :
             pass
 
 # https://www.youtube.com/watch?v=NoltgGRat2Y, https://www.youtube.com/watch?v=u_zeA_UFRnA
     def handleSearch(self):
+        self.videoCards = [] # reset value to empty list
         value = self.inputBox.text().strip()
         urls = value.split(",")
         videoInfo = []
-        for url in urls:
+        for i,url in enumerate(urls):
             url = url.strip()
             if len(url) > 0:
-                videoInfo.append(self.getVideoInfo(url))
-        videoCatalog = CustomList(self.filter,videoInfo)
+                (thumbnail,title) = self.getVideoInfo(url)
+                if thumbnail and title:
+                    self.videoCards.append({"id":i,"url":url})
+                    videoInfo.append((thumbnail,title))
+        videoCatalog = CustomList(self.filter,videoInfo,self.videoCards)
         self.mainLayout.addWidget(videoCatalog)
                 
     def createSearchBtn(self,inputBox,filter):
         self.filter = filter
         downloadBtn = self.filter.widgets[1]
         def handleDownload():
-            for yt in self.youtube:
-                stream = yt.streams.filter(progressive=True,resolution="720p").first()
-                a = stream.download("/home/naren/Downloads")
-                print("stream",stream)
-                # stream = yt.streams.filter(progressive=True,resolution="720p").first()
-                # downloadedVideo = stream.download(output_path="/home/naren/Downloads")
-                # print(f"video downloaded:", downloadedVideo)
-            pass
+            print("video cards")
+            print(self.videoCards)
+            # before starting download check the list of selected file to be downloaded 
+            # only selected file are allowed to be download
+            # if self.filter.selectAll == True then no need to check for each file to be downloaded
+            # just download every file
+            # elif self.filter.selectAll == False then only download those file that are checked
+            for i,card in enumerate(self.videoCards):
+                url = card["url"]
+                isChecked = card["checkState"]
+                print(url,isChecked)
+                if url and isChecked:
+                    yt = YouTube(url)
+                    createThread(yt)
         if downloadBtn:
             downloadBtn.clicked.connect(handleDownload)
         self.inputBox = inputBox
@@ -58,3 +70,15 @@ class SearchBtn():
         btn.setStyleSheet(f"padding:{padding}px;")
         btn.clicked.connect(self.handleSearch)
         return btn
+
+
+def createThread(yt):
+    thread = threading.Thread(target=download,args=[yt])
+    thread.start()
+    
+
+def download(yt:YouTube):
+    stream = yt.streams.filter(progressive=True,resolution="720p").first()
+    folderPath = generateFolderPath()
+    downloadPath = stream.download(folderPath)
+    print("download path",downloadPath)
